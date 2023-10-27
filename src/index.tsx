016409e76902e9be34b0f7d269b566884a9b343d
GreatArchimage <18934074140@163.com>
2023-10-27 21:43:36 +0800

    配置路径别名

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import sum from '@/test';
+
+var a = sum(2,3)
+console.log(a)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
